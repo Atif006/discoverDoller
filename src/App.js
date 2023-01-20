39e00885ac6bc0316cfd79376333baeb39580481
MD IRFAN <53875348+Atif006@users.noreply.github.com>
2023-01-21 00:56:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import Header from "./header/Header";
+import Home from "./home/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
